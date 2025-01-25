74576252da4141e8b564c85193c3273c38ee6aba
Mikhail Klyshnikov <mishakurg@gmail.com>
2025-01-25 22:51:28 +0300

    add fucs to command manager

--- tgbot/TgBot/TgBot/Commands/CommandManager.cs
+++ tgbot/TgBot/TgBot/Commands/CommandManager.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using TgBot.Commands.Models;
 
 namespace TgBot.Commands;
 
 internal class CommandManager
 {
+    internal static Dictionary<CommandAvailabilityScope, List<BotCommand>> BotCommands { get; } = GetBotCommands();
 
+    private static Dictionary<CommandAvailabilityScope, List<BotCommand>> GetBotCommands()
+    { }
 }
