e466e4e2c14c0e8effc1847e6ad14972f5d7a115
Mikhail Klyshnikov <mishakurg@gmail.com>
2025-01-26 00:05:35 +0300

    add reflection get attributes

--- tgbot/TgBot/TgBot/Commands/CommandManager.cs
+++ tgbot/TgBot/TgBot/Commands/CommandManager.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot.Types;
 using TgBot.Commands.Models;
+using TgBot.Commands.Interfaces;
+using System.Reflection;
+using System.Data;
+using TgBot.Commands.Attributes;
 
 namespace TgBot.Commands;
 
@@ -13,5 +17,18 @@ internal class CommandManager
     internal static Dictionary<CommandAvailabilityScope, List<BotCommand>> BotCommands { get; } = GetBotCommands();
 
     private static Dictionary<CommandAvailabilityScope, List<BotCommand>> GetBotCommands()
-    { }
+    { 
+        
+    }
+
+    private static Dictionary<string, (string, CommandAvailabilityScope, IBotCommand)> GetCommands()
+    { 
+        return Assembly
+            .GetExecutingAssembly()
+            .GetTypes()
+            .Where(t => t.IsAssignableTo(typeof(IBotCommand)))
+            .Select(t => (CommandType: t, BotCommand: t.GetCustomAttribute<BotCommandAttribute>()))
+            .Where(ta => ta.BotCommand != null)
+            .ToDictionary(ta => "/" + ta.BotCommand!.Command, ta => (ta.BotCommand!.Description, ta.BotCommand.AvailabilityScope, (IBotCommand) Activator.CreateInstance(ta.CommandType)!))
+    }
 }
