7ee46f8a661a1bec6107ed696856c59a72cb1c21
Mikhail Klyshnikov <mishakurg@gmail.com>
2025-01-25 15:47:38 +0300

    start creating /help command

---
+++ tgbot/TgBot/TgBot/Commands/HelpCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TgBot.Commands;
+
+[BotCommand("help", "")]
+internal class HelpCommand : ICommand
+{
+
+}
--- tgbot/TgBot/TgBot/Program.cs
+++ tgbot/TgBot/TgBot/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot.Types;
+
+namespace TgBot;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Environment.CurrentDirectory = AppContext.BaseDirectory;
+    }
+}
+
