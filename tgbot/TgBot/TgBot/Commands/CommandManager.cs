dcb12c3520dc726f0d89dd60d8942047c3cdf5a8
Mikhail Klyshnikov <mishakurg@gmail.com>
2025-01-26 00:20:58 +0300

    release AddBotCommand

--- tgbot/TgBot/TgBot/Commands/CommandManager.cs
+++ tgbot/TgBot/TgBot/Commands/CommandManager.cs
@@ -26,11 +26,11 @@ internal class CommandManager
             {
                 case CommandAvailabilityScope.Chat:
                 case CommandAvailabilityScope.Private:
-                    AddBotCommand(commandData.Item2, commandName, commandData.Item1);
+                    AddBotCommand(commandData.Item2, commandName, commandData.Item1, result);
                     break;
                 case CommandAvailabilityScope.Chat | CommandAvailabilityScope.Private:
-                    AddBotCommand(CommandAvailabilityScope.Chat, commandName, commandData.Item1);
-                    AddBotCommand(CommandAvailabilityScope.Private, commandName, commandData.Item1);
+                    AddBotCommand(CommandAvailabilityScope.Chat, commandName, commandData.Item1, result);
+                    AddBotCommand(CommandAvailabilityScope.Private, commandName, commandData.Item1, result);
                     break;
             }
         }
@@ -39,13 +39,27 @@ internal class CommandManager
     }
 
     private static Dictionary<string, (string, CommandAvailabilityScope, IBotCommand)> GetCommands()
-    { 
+    {
         return Assembly
             .GetExecutingAssembly()
             .GetTypes()
             .Where(t => t.IsAssignableTo(typeof(IBotCommand)))
             .Select(t => (CommandType: t, BotCommand: t.GetCustomAttribute<BotCommandAttribute>()))
             .Where(ta => ta.BotCommand != null)
-            .ToDictionary(ta => "/" + ta.BotCommand!.Command, ta => (ta.BotCommand!.Description, ta.BotCommand.AvailabilityScope, (IBotCommand) Activator.CreateInstance(ta.CommandType)!))
+            .ToDictionary(ta => "/" + ta.BotCommand!.Command, ta => (ta.BotCommand!.Description, ta.BotCommand.AvailabilityScope, (IBotCommand)Activator.CreateInstance(ta.CommandType)!));
+    }
+
+    private static void AddBotCommand(CommandAvailabilityScope commandAvailabilityScope, string commad, string description, Dictionary<CommandAvailabilityScope, List<BotCommand>> commandsDictionary)
+    {
+        var c = new BotCommand { Command = commad, Description = description };
+
+        if (commandsDictionary!.TryGetValue(commandAvailabilityScope, out List<BotCommand>? list))
+        {
+            list.Add(c);
+        }
+        else 
+        {
+            commandsDictionary[commandAvailabilityScope] = new List<BotCommand> { c };
+        }
     }
 }
