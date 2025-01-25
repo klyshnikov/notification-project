95b08429cc721473a739bb1fd9aa3fbc08f7e180
Mikhail Klyshnikov <mishakurg@gmail.com>
2025-01-25 22:02:23 +0300

    continue bot wrapper - add inner bot client

--- tgbot/TgBot/TgBot/Bot/Bot.cs
+++ tgbot/TgBot/TgBot/Bot/Bot.cs
@@ -3,9 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using TgBot.Commands;
+using TgBot.Commands.Models;
+using Telegram.Bot;
 
 namespace TgBot.Bot;
 
 internal class Bot
 {
+    private readonly TelegramBotClient _client;
+    private readonly HttpClient _httpClient;
+
+    internal Bot()
+    { 
+        _httpClient = new HttpClient();
+        _client = new TelegramBotClient("TOKEN", _httpClient);
+    }
+
+    internal async Task<string> Start()
+    {
+        foreach ((CommandAvailabilityScope availabilityScope, List<BotCommand> commands) in CommandManager.BotCommands)
+        {
+            switch (availabilityScope)
+            { 
+                case CommandAvailabilityScope.Private:
+                    await _client.
+            }
+        }
+    }
 }
