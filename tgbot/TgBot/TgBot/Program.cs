949c9cdaa585f948a9588bc2eed00059531d89fa
Mikhail Klyshnikov <mishakurg@gmail.com>
2025-01-26 01:04:38 +0300

    add logic to Progam

--- tgbot/TgBot/TgBot/Program.cs
+++ tgbot/TgBot/TgBot/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot.Types;
+using TgBot.Bot;
 
 namespace TgBot;
 
@@ -10,6 +11,10 @@ public static class Program
     public static void Main(string[] args)
     {
         Environment.CurrentDirectory = AppContext.BaseDirectory;
+
+        var _bot = new TelegramBot();
+        Task.FromResult(_bot.Start());
+        Console.ReadLine();
     }
 }
 
