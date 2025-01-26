8677d096a4101dcafb3a5cb93420f8d2393a9ee4
Mikhail Klyshnikov <mishakurg@gmail.com>
2025-01-26 22:47:47 +0300

    add some logic to set_team command

--- tgbot/TgBot/TgBot/Commands/SetTeamCommand.cs
+++ tgbot/TgBot/TgBot/Commands/SetTeamCommand.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot.Types;
 using TgBot.Commands.Attributes;
 using TgBot.Commands.Interfaces;
 using TgBot.Models;
+using TgBot.Settings;
 
 namespace TgBot.Commands;
 
@@ -15,8 +17,19 @@ internal class SetTeamCommand : IBotCommand
 {
     public async Task<BotResponse?> ExecuteAsync(Message message, BotOptions botOptions, CancellationToken cancellationTocken)
     {
-
+        string[] commandArgs = message.Text.Split(' ', 2);
+        string teamName = commandArgs[0];
+        var userId = message.From;
         Console.WriteLine("Command inside");
-        return await Task.FromResult<BotResponse?>(new BotResponse("Hello")).ConfigureAwait(false);
+
+        using HttpClient httpClient = new HttpClient();
+
+        httpClient.DefaultRequestHeaders.Add("Team-Members", teamMembersIds);
+
+        httpClient.BaseAddress = new Uri(SettingsManager.SERVICE_ADDRESS);
+
+        var res = httpClient.GetFromJsonAsync<>("/set-team");
+
+        return await Task.FromResult<BotResponse?>(new BotResponse("Для данной группы успешно создана команда")).ConfigureAwait(false);
     }
 }
