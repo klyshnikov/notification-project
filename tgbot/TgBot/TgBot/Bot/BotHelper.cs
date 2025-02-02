fe3f9bd589b2dcb93e553dc0d6afcaffc16d4b34
Mikhail Klyshnikov <mishakurg@gmail.com>
2025-02-02 22:42:19 +0300

    add helper

---
+++ tgbot/TgBot/TgBot/Bot/BotHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TgBot.Bot;
+
+internal class BotHelper
+{
+    private static TelegramBotClient _client;
+
+    public static void SetClient(TelegramBotClient client)
+    { 
+        _client = client;
+    }
+
+    public static async Task<ChatMember> GetChatMembers(ChatId chatId, long userId)
+    {
+        return await _client.GetChatMember(chatId, userId);
+    }
+}
