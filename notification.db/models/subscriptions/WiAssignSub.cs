11c633f7e0f8a124c22afa28ce9429265d3bcb0d
Mikhail Klyshnikov <mishakurg@gmail.com>
2025-05-19 09:56:25 +0300

    add sub models to db

---
+++ notification.db/models/subscriptions/WiAssignSub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace models.subscriptions;
+
+[Table("wi_assign_sub")]
+public class WiAssignSub
+{
+    [Column("user_id", Order = 0)]
+    public string UserId { get; set; }
+}
---
+++ notification.db/models/subscriptions/WiTimeRemainingSub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace models.subscriptions;
+
+[Table("wi_time_remaining_sub")]
+public class WiTimeRemainingSub
+{
+    [Column("user_id", Order = 0)]
+    public string UserId { get; set; }
+
+    [Column("minutes_remaining", Order = 1)]
+    public string MinutesRemaining { get; set; }
+}
