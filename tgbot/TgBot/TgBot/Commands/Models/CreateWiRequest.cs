4b28dc6caff041aa3838ba438fbea813b2c53c0f
Mikhail Klyshnikov <mishakurg@gmail.com>
2025-05-19 10:07:15 +0300

    add command models

---
+++ tgbot/TgBot/TgBot/Commands/Models/CreateWiRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TgBot.Commands.Models;
+
+public class CreateWiRequest
+{
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime StartTime { get; set; }
+
+    public DateTime EndTime { get; set; }
+
+    public string AuthorId { get; set; }
+
+    public DateTime CreatedTime { get; set; }
+
+    public string ChatId { get; set; }
+}
